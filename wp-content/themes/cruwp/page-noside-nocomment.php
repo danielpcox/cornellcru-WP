58dfb787ad45e5dd369fbedc7020450e7af1bb01
Daniel Cox <danielpcox@gmail.com>
2010-07-21 00:23:07 -0500

    fixed the fact that page-noside-nocomment wasn't loading

--- wp-content/themes/cruwp/page-noside-nocomment.php
+++ wp-content/themes/cruwp/page-noside-nocomment.php
@@ -4,37 +4,47 @@ Template Name:No sidebar, No comment
 */
 ?>
 <?php get_header(); ?>
-  <div id="no-side" class="clearfix">
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div id="main-cont">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php
+    if($post->post_parent) {
+    $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+    $titlenamer = get_the_title($post->post_parent);
+    }
 
-    <div class="post" id="single">
-     <h2><?php the_title(); ?></h2>
-     <div class="post-content">
-       <?php the_content(__('Read more', 'cruwp')); ?>
-       <?php wp_link_pages(); ?>
-     </div>
-    </div>
+    else {
+    $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+    $titlenamer = get_the_title($post->ID);
+    }
+    if ($children) { ?>
+      <h2> <? echo $titlenamer ?> </h2>
 
-<?php endwhile; else: ?>
-    <div class="post-content">
-      <p><?php _e("Sorry, but you are looking for something that isn't here.","cruwp"); ?></p>
-    </div>
-<?php endif; ?>
+      <div id="sidebar">
+        <ul class="nav">
+          <?php echo $children; ?>
+        </ul><!--/.nav-->
+      </div><!--/#sidebar-->
 
-  </div><!-- #middle-contents end -->
 
-  <div id="footer-noside">
-  </div>
- 
- </div><!-- #contents end -->
-</div><!-- #wrapper end -->
+      <h1>I Am New Here</h1>
 
-<?php $options = get_option('pb_options'); if ($options['return_top']) : ?>
-<div id="page-top">
- <a href="#wrapper">&nbsp;</a>
+      <div id="posts">
+            <div class="post">
+                test
+            </div><!--/#posts-->
+      </div>
+  <?php } ?>
+  <?php endwhile; else: ?>
+      <h1>Page Not Found</h1>
+      <div id="posts">
+        <div class="post">
+          <p><?php _e("Sorry, but you are looking for something that isn't here.","cruwp"); ?></p>
+        </div>
+      </div><!--/.post-->
+  <?php endif; ?>
+
 </div>
-<?php endif; ?>
 
-</body>
-</html>
+
+<?php get_footer(); ?>
