dd84e725b85809235f88362b785c4705e53cbfe5
Daniel Cox <danielpcox@gmail.com>
2010-08-05 19:51:29 -0500

    added Google Analytics code to header

--- wp-content/themes/cruwp/header.php
+++ wp-content/themes/cruwp/header.php
@@ -39,6 +39,20 @@
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/easySlider1.7.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/application.js"></script>
+<!-- Google Analytics -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17839609-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
     <div id="header">
