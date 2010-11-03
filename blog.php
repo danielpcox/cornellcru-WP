efc19cc20b3f6a8451dc43aa8d85ef560a36c568
Sarah Elliott <sse25@cornell.edu>
2010-11-03 18:12:00 -0400

    added the blog.php file

---
+++ blog.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head profile="http://gmpg.org/xfn/11"> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" /> 
+<title>Cornell Campus Crusade for Christ</title> 
+<meta name="description" content="" /> 
+<link rel="alternate" type="application/rss+xml" title="Cornell Campus Crusade for Christ RSS Feed" href="http://cornellcru.com/feed/" /> 
+<link rel="alternate" type="application/atom+xml" title="Cornell Campus Crusade for Christ Atom Feed" href="http://cornellcru.com/feed/atom/" /> 
+<link rel="pingback" href="http://cornellcru.com/xmlrpc.php" /> 
+ 
+<link rel="stylesheet" href="http://cornellcru.com/wp-content/themes/cruwp/style.css" type="text/css" media="screen" /> 
+<link rel="stylesheet" href="http://cornellcru.com/wp-content/themes/cruwp/comment-style.css" type="text/css" media="screen" /> 
+ 
+<!--[if lt IE 7]>
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/iepngfix.js"></script>
+<link rel="stylesheet" href="http://cornellcru.com/wp-content/themes/cruwp/ie6.css" type="text/css" media="screen" />
+<![endif]--> 
+ 
+<style type="text/css"> 
+.post img, .post a img { border:1px solid #222; padding:5px; margin:0;  background:#555; }
+.post a:hover img { border:1px solid #849ca0; background:#59847d; }
+.post img.wp-smiley { border:0px; padding:0px; margin:0px; background:none; }
+</style> 
+ 
+ 
+<script type='text/javascript' src='http://cornellcru.com/wp-includes/js/jquery/jquery.js?ver=1.4.2'></script> 
+<style type="text/css">img.nothumb{border:0;margin:0 0 0 2px !important;}</style> 
+<!-- Tweet This v1.8 b034 mode alpha zeta --> 
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://cornellcru.com/xmlrpc.php?rsd" /> 
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://cornellcru.com/wp-includes/wlwmanifest.xml" /> 
+<link rel='index' title='Cornell Campus Crusade for Christ' href='http://cornellcru.com/' /> 
+<meta name="generator" content="WordPress 3.0.1" /> 
+<link rel='shortlink' href='http://j.mp/9NMtUJ' /> 
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/scroll.js"></script> 
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/jscript.js"></script> 
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/comment.js"></script> 
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/jquery.js"></script> 
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/easySlider1.7.js"></script> 
+<script type="text/javascript" src="http://cornellcru.com/wp-content/themes/cruwp/js/application.js"></script> 
+<!-- Google Analytics --> 
+<script type="text/javascript"> 
+ 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17839609-1']);
+  _gaq.push(['_trackPageview']);
+ 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+ 
+</script> 
+</head> 
+<body> 
+    <div id="header"> 
+      <a id="logo" href="http://cornellcru.com/">Cornell Campus Crusade for Christ</a><!--/#logo--> 
+                           <form method="get" id="search" action="http://cornellcru.com/"> 
+           <input type="text" value="Search" name="s" id="search-box" /> 
+          </form> 
+                     </div><!--/#header--> 
+ 
+    <ul id="navigation"> 
+      <li class="current_page_item"><a href="http://cornellcru.com/">HOME</a></li> 
+      <li class="page_item"><a href="http://cornellcru.com/im-new-here/">I'm New Here</a></li> 
+      <li class="page_item"><a href="http://cornellcru.com/news-and-events/">News and Events</a></li> 
+      <li class="page_item"><a href="http://cornellcru.com/following-jesus/">Following Jesus</a></li> 
+      <li class="page_item"><a href="http://cornellcru.com/about-us/">About Us</a></li> 
+      <li class="page_item"><a href="http://labs.cornellcru.com/sse25/blog.php">Blog</a></li>
+      <!--<li class="page_item page-item-25"><a href="http://cornellcru.com/real-life/" title="Real Life">Real Life</a></li>
+<li class="page_item page-item-27"><a href="http://cornellcru.com/jesus-and-the-gospel/" title="Jesus and the Gospel">Jesus and the Gospel</a></li>
+<li class="page_item page-item-29"><a href="http://cornellcru.com/community-groups/" title="Community Groups">Community Groups</a>
+<ul class='children'>
+	<li class="page_item page-item-161"><a href="http://cornellcru.com/community-groups/mens-groups/" title="Men&#8217;s Groups">Men&#8217;s Groups</a></li>
+	<li class="page_item page-item-163"><a href="http://cornellcru.com/community-groups/womens-groups/" title="Women&#8217;s Groups">Women&#8217;s Groups</a></li>
+	<li class="page_item page-item-165"><a href="http://cornellcru.com/community-groups/co-ed-groups/" title="Co-Ed Groups">Co-Ed Groups</a></li>
+	<li class="page_item page-item-469"><a href="http://cornellcru.com/community-groups/grad-student-groups/" title="Grad Student Groups">Grad Student Groups</a></li>
+</ul>
+</li>
+<li class="page_item page-item-287"><a href="http://cornellcru.com/the-mott-house/" title="The Mott House">The Mott House</a></li>
+<li class="page_item page-item-108"><a href="http://cornellcru.com/satisfaction-video/" title="Satisfaction">Satisfaction</a></li>
+<li class="page_item page-item-212"><a href="http://cornellcru.com/calendar/" title="Calendar">Calendar</a></li>
+<li class="page_item page-item-245"><a href="http://cornellcru.com/upstate-fall-retreat/" title="Upstate Fall Retreat">Upstate Fall Retreat</a></li>
+<li class="page_item page-item-314"><a href="http://cornellcru.com/mailing-list-stuff/" title="Mailing List Stuff">Mailing List Stuff</a></li>
+<li class="page_item page-item-519"><a href="http://cornellcru.com/prayer-meetings/" title="Prayer Meetings">Prayer Meetings</a></li>
+<li class="page_item page-item-621"><a href="http://cornellcru.com/iron-man/" title="IronMan">IronMan</a></li>
+<li class="page_item page-item-7"><a href="http://cornellcru.com/im-new-here/" title="I’m New Here">I’m New Here</a>
+<ul class='children'>
+	<li class="page_item page-item-130"><a href="http://cornellcru.com/im-new-here/find-a-church/" title="Find A Church">Find A Church</a></li>
+	<li class="page_item page-item-183"><a href="http://cornellcru.com/im-new-here/campus-ministries/" title="Campus Ministries">Campus Ministries</a></li>
+	<li class="page_item page-item-413"><a href="http://cornellcru.com/im-new-here/everystudent-com-2/" title="Everystudent.com">Everystudent.com</a></li>
+</ul>
+</li>
+<li class="page_item page-item-9"><a href="http://cornellcru.com/news-and-events/" title="News and Events">News and Events</a></li>
+<li class="page_item page-item-11"><a href="http://cornellcru.com/following-jesus/" title="Following Jesus">Following Jesus</a>
+<ul class='children'>
+	<li class="page_item page-item-252"><a href="http://cornellcru.com/following-jesus/bible/" title="Bible">Bible</a></li>
+	<li class="page_item page-item-254"><a href="http://cornellcru.com/following-jesus/prayer/" title="Prayer">Prayer</a></li>
+	<li class="page_item page-item-256"><a href="http://cornellcru.com/following-jesus/fellowship/" title="Fellowship">Fellowship</a></li>
+</ul>
+</li>
+<li class="page_item page-item-13"><a href="http://cornellcru.com/about-us/" title="About Us">About Us</a>
+<ul class='children'>
+	<li class="page_item page-item-117"><a href="http://cornellcru.com/about-us/cru-staff/" title="Cru Staff">Cru Staff</a></li>
+	<li class="page_item page-item-428"><a href="http://cornellcru.com/about-us/contact-us/" title="Contact Us">Contact Us</a></li>
+</ul>
+</li>
+--><!-- using exclude pages as if they were actually INCLUDE pages --> 
+    </ul><!--/#navigation--> 
+
+
+<div id="blog"> 
+  <h1>The Cru Blog</h1> 
+ 
+  <!-- original "subscribe" link
+      <a href="http://cornellcru.com/feed/" id="blog-subscribe" >subscribe</a>
+  --> 
+ 
+  <ul id="filters"> 
+              <li><a href="http://cornellcru.com/feed/" >feed</a></li> 
+                      <li><a href="http://twitter.com/cornellcru" id="twitter" >twitter</a></li> 
+        </ul><!--/#filters--> 
+ 
+  <ul id="posts"> 
+          <li class="post"> 
+          <h2><a href="http://cornellcru.com/2010/10/pulling-out-teeth/">Pulling Out Teeth</a></h2> 
+          <ul class="meta"> 
+            <li class="date">October 26th, 2010</li> 
+            <!--<li class="author">by <a href="http://cornellcru.com/author/william-poon/" title="Posts by William Poon">William Poon</a></li>--><!-- TODO : enable author link? --> 
+            <li class="author">by William Poon</li>            <!--<li class="comments"><a href="http://cornellcru.com/2010/10/pulling-out-teeth/#respond" title="Comment on Pulling Out Teeth">Write comment</a></li>--><!-- TODO : enable comments --> 
+                      </ul><!--/.meta--> 
+       <p>Have you ever been a part of a fundraiser? Have you ever had to sell raffles for a good cause, your sports team, your school? If you have, you’ll probably agree with me that it sucks. It’s one of the most stressful things a person can do. It’s like trying to pull the teeth out of every single person you know without any anesthetics, there’s always screaming, yelling, squirming, and hard feelings. And just think of all the people who do that kind of stuff for a living. How do they do it? I’m pretty sure I would be bald by now if I had to do it for a living from pulling my hair out wondering “Why is it so hard for people to give to a good cause?” It usually includes trying to guilt trip people into giving or giving them something in return for their money, in which it becomes a business. <a href="http://cornellcru.com/2010/10/pulling-out-teeth/#more-629" class="more-link">[...more]</a></p> 
+<div class="tweetthis" style="text-align:left;"><p> <a target="_blank" rel="nofollow" class="tt" href="http://twitter.com/home/?status=Pulling+Out+Teeth+http%3A%2F%2Fj.mp%2F9NMtUJ" title="Post to Twitter"><img class="nothumb" src="http://cornellcru.com/wp-content/plugins/tweet-this/icons/en/twitter/tt-twitter4.png" alt="Post to Twitter" /></a></p></div>              <!--<a href="http://cornellcru.com/category/uncategorized/" title="View all posts in Uncategorized" rel="category tag">Uncategorized</a>--><!-- TODO : enable categories? --> 
+       <!----> <!-- TODO : enable tags? --> 
+       
+      </li><!--/.post--> 
+          <li class="post"> 
+          <h2><a href="http://cornellcru.com/2010/10/a-heart-for-the-lost-a-heart-of-compassion/">A Heart for the Lost, A Heart of Compassion</a></h2> 
+          <ul class="meta"> 
+            <li class="date">October 21st, 2010</li> 
+            <!--<li class="author">by <a href="http://cornellcru.com/author/edgarlei/" title="Posts by edgarlei">edgarlei</a></li>--><!-- TODO : enable author link? --> 
+            <li class="author">by edgarlei</li>            <!--<li class="comments"><a href="http://cornellcru.com/2010/10/a-heart-for-the-lost-a-heart-of-compassion/#respond" title="Comment on A Heart for the Lost, A Heart of Compassion">Write comment</a></li>--><!-- TODO : enable comments --> 
+                      </ul><!--/.meta--> 
+       <p>At the end of Tim Keller’s book “Prodigal God”, he makes a very powerful point that has stuck with me since I read it last.  You may be familiar with the parable that Jesus told in Luke 15 about a Father and his two sons, the younger of whom runs to a distant country and spends all his inheritance on wild living, and then makes his way back to the Father, who not only forgives the affront but welcomes him back warmly and with love.  The Father loved his son so much, and as if to explain the celebration, he says “For this son of mine was dead and is alive again; <em>he was lost and is found</em>.” <a href="http://cornellcru.com/2010/10/a-heart-for-the-lost-a-heart-of-compassion/#more-545" class="more-link">[...more]</a></p> 
+<div class="tweetthis" style="text-align:left;"><p> <a target="_blank" rel="nofollow" class="tt" href="http://twitter.com/home/?status=A+Heart+for+the+Lost%2C+A+Heart+of+Compassion+http%3A%2F%2Fj.mp%2FcGBzfI" title="Post to Twitter"><img class="nothumb" src="http://cornellcru.com/wp-content/plugins/tweet-this/icons/en/twitter/tt-twitter4.png" alt="Post to Twitter" /></a></p></div>              <!--<a href="http://cornellcru.com/category/uncategorized/" title="View all posts in Uncategorized" rel="category tag">Uncategorized</a>--><!-- TODO : enable categories? --> 
+       <!----> <!-- TODO : enable tags? --> 
+       
+      </li><!--/.post--> 
+          <li class="post"> 
+          <h2><a href="http://cornellcru.com/2010/10/if-you-wish-i-will-make-three-tents-here/">&#8220;If You Wish, I Will Make Three Tents Here&#8221;</a></h2> 
+          <ul class="meta"> 
+            <li class="date">October 19th, 2010</li> 
+            <!--<li class="author">by <a href="http://cornellcru.com/author/larrylin/" title="Posts by Larry Lin">Larry Lin</a></li>--><!-- TODO : enable author link? --> 
+            <li class="author">by Larry Lin</li>            <!--<li class="comments"><a href="http://cornellcru.com/2010/10/if-you-wish-i-will-make-three-tents-here/#respond" title="Comment on &#8220;If You Wish, I Will Make Three Tents Here&#8221;">Write comment</a></li>--><!-- TODO : enable comments --> 
+                      </ul><!--/.meta--> 
+       <p>I used to think that Peter was one of the most hilarious people in the Bible. I now realize that it was usually because I didn&#8217;t really understand him. I still think he&#8217;s a little bit hilarious, though.</p> 
+<p>Here&#8217;s one example. In Matthew 17, Jesus takes Peter, James, and John up to the top of this mountain, where Jesus&#8217; face is shining like the sun, and Moses and Elijah appear. If that&#8217;s not a life-transforming moment, I don&#8217;t know what is. And so Peter, full of words of wisdom, says, &#8220;Lord, it is good that we are here. If you wish, I will make three tents here, one for you and one for Moses and one for Elijah.&#8221;</p> 
+<p>&#8220;If you wish, I will make three tents here.&#8221; Are you kidding me? <a href="http://cornellcru.com/2010/10/if-you-wish-i-will-make-three-tents-here/#more-601" class="more-link">[...more]</a></p> 
+<div class="tweetthis" style="text-align:left;"><p> <a target="_blank" rel="nofollow" class="tt" href="http://twitter.com/home/?status=%E2%80%9CIf+You+Wish%2C+I+Will+Make+Three+Tents+Here%E2%80%9D+http%3A%2F%2Fj.mp%2Fcs781Q" title="Post to Twitter"><img class="nothumb" src="http://cornellcru.com/wp-content/plugins/tweet-this/icons/en/twitter/tt-twitter4.png" alt="Post to Twitter" /></a></p></div>              <!--<a href="http://cornellcru.com/category/uncategorized/" title="View all posts in Uncategorized" rel="category tag">Uncategorized</a>--><!-- TODO : enable categories? --> 
+       <!----> <!-- TODO : enable tags? --> 
+       
+      </li><!--/.post--> 
+      </ul><!--/#posts--> 
+  
+</div><!--/#blog--> 
+ 
+<ul id="pagination"> 
+<span class='page-numbers current'>1</span> 
+<a class='page-numbers' href='http://cornellcru.com/page/2/'>2</a> 
+<a class='page-numbers' href='http://cornellcru.com/page/3/'>3</a> 
+<span class='page-numbers dots'>...</span> 
+<a class='page-numbers' href='http://cornellcru.com/page/7/'>7</a> 
+<a class='next page-numbers' href='http://cornellcru.com/page/2/'>Older &raquo;</a> 
+</ul> 
+ 
+<!-- We're gonna have to totally redo the pagination styling for wordpress
+<ul id="pagination">
+</ul>
+--> 
+ 
+ 
+ <!-- TODO: style the sidebar --> 
+<div id="return_top"> 
+ <a href="#header">&nbsp;</a> 
+</div> 
+ 
+<div id="footer"> 
+  <span id="copy">&copy; Copyright 2010</span><!--/#copy--> 
+  <br /> 
+  <span id="name">Cornell Campus Crusade for Christ</span><!--/#name--> 
+  <script src="http://bible.logos.com/jsapi/Referencetagging.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+			Logos.ReferenceTagging.lbsBibleVersion = "ESV";
+
+			Logos.ReferenceTagging.lbsLibronixBibleVersion = "ESV";
+
+									Logos.ReferenceTagging.lbsLibronixLinkIcon = "light";
+
+									Logos.ReferenceTagging.lbsNoSearchTagNames = [ "h1", "h2", "h3" ];
+
+						Logos.ReferenceTagging.lbsCssOverride = true;									Logos.ReferenceTagging.tag();
+
+		</script>
+
+</div><!--/#footer--> 
+</body> 
+</html> 
